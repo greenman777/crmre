0eb0dd454fcd049181bdfb4c6ad8596731512594
greenman777 <chaos777@mail.ru>
2017-01-18 16:42:48 +0300

    Check size message for Notifications

--- crmreapp/static/js/app/controller/OrdersSale.js
+++ crmreapp/static/js/app/controller/OrdersSale.js
@@ -580,7 +580,7 @@ Ext.define('CRMRE.controller.OrdersSale', {
                                     if (success) {
                                         client = records[0];
                                         var message = 'Вам новая заявка № '+selection_orders_sale[0].get('index')+": "+selection_orders_sale[0].get('heading')+", от "+client.get('represent')+" т."+client.get('phone_represent');
-                                        my.fireEvent('addNotifications',performer,message);
+                                        my.fireEvent('addNotifications',performer,message.slice(0,150));
                                         //my.fireEvent('addTask',performer,'Отработать заявку № '+selection_orders_sale[0].get('index'),'Посмотреть, сфотографировать, заполнить данные заявки');
                                     }
                                 }});
@@ -605,7 +605,7 @@ Ext.define('CRMRE.controller.OrdersSale', {
                                     if (success) {
                                         client = records[0];
                                         var message = 'Вам новая заявка № '+selection_orders_sale[0].get('index')+": "+selection_orders_sale[0].get('heading')+", от "+client.get('represent')+" т."+client.get('phone_represent');
-                                        my.fireEvent('addNotifications',performer,message);
+                                        my.fireEvent('addNotifications',performer,message.slice(0,150));
                                         //my.fireEvent('addTask',performer,'Отработать заявку № '+selection_orders_sale[0].get('index'),'Посмотреть, сфотографировать, заполнить данные заявки');
                                     }
                                 }});
@@ -713,7 +713,7 @@ Ext.define('CRMRE.controller.OrdersSale', {
                                 if (success) {
                                     client = records[0];
                                     var message = 'Вам новая заявка № '+record.get('index')+": "+record.get('heading')+", от "+client.get('represent')+" т."+client.get('phone_represent');
-                                    my.fireEvent('addNotifications',record.get('performer'),message);
+                                    my.fireEvent('addNotifications',record.get('performer'),message.slice(0,150));
                                 }
                             }});
                         };
